97e20bf139fef502b2992b1fdd9cf7c8928588b8
Luke Shumaker <lukeshu@datawire.io>
2021-01-29 10:38:37 -0700

    dhttp: Doc and comment improvements based on feedback from Flynn

--- dhttp/server.go
+++ dhttp/server.go
@@ -47,8 +47,15 @@ import (
 	"github.com/datawire/dlib/dlog"
 )
 
+// connContextFn is just a convenience type alias because the type signature for concatConnContext
+// would be really hard to read without it.  It is just a name for the type of the struct members
+// ServerConfig.ConnContext and http.Server.ConnContext.
 type connContextFn func(ctx context.Context, c net.Conn) context.Context
 
+// concatConnContext takes a list of zero or more callback-functions that would each be suitable as
+// a value for ServerConfig.ConnContext (or http.Server.ConnContext), and concatenates them together
+// in to one callback-function.  The input callback-functions will be run in the order that they're
+// passed to concatConnContext.
 func concatConnContext(fns ...connContextFn) connContextFn {
 	return func(ctx context.Context, c net.Conn) context.Context {
 		for _, fn := range fns {
@@ -64,20 +71,42 @@ func concatConnContext(fns ...connContextFn) connContextFn {
 	}
 }
 
+// testHookContextKey is a hack so that some of the tests can hook in to the Handler internals a
+// bit, via serverhook_test.go.
 type testHookContextKey struct{}
 
 // ServerConfig is a mostly-drop-in replacement for net/http.Server.
 //
 // This is better than http.Server because:
 //
-//  - It natively supports "h2c" (HTTP/2 over cleartext)
+//  - It natively supports "h2c" (HTTP/2 over cleartext).
+//
 //  - Its "h2c" support is better than http.Server with golang.org/x/net/http2/h2c.NewHandler,
-//    because it properly shuts down idle h2c connections when server.Shutdown is called, rather
-//    than allowing h2c connections to sit around forever.
+//    because it properly shuts down idle h2c connections when shutdown is initiated, rather than
+//    allowing h2c connections to sit around forever.
+//
 //  - It uses Context cancellation as a simple and composable shutdown mechanism; supporting
 //    dcontext hard/soft contexts, rather than awkward relationship between the Shutdown and Close
 //    methods.
+//
+//    Rather than having to set up a tree of cleanup functions calling each other down through your
+//    program and dealing with having to call .Shutdown() and .Close() from another goroutine (and
+//    having to understand the relationship between .Shutdown() and .Close() and when to call each,
+//    which is a bigger task than you might think), the "(ListenAnd)?Serve(TLS)?" methods simply
+//    take a Context and perform cleanup when the Context becomes Done.  In order to differentiate
+//    between whether you want it to hard-shutdown or graceful-shutdown, you may use the dcontext
+//    hard/soft mechanism; if you don't use dcontext then it will be a hard-shutdown.
+//
 //  - When shutting down, it properly blocks when waiting for the workers of hijacked connections.
+//
+//    Hijacked connections are connections for which the .Handler said to the server "you know what,
+//    stop doing HTTP and let me use the raw TCP socket" (as when having negotiated an upgrade from
+//    HTTP/1 to WebSockets or from HTTP/1 to HTTP/2).  It is a design deficiency in net/http.Server
+//    that the net/http.Server totally just stops tracking connections when they get hijacked;
+//    failing to close them when you call net/http.Server.Close() and failing to wait for them when
+//    you call net/http.Server.Shutdown(); and unfortunately that design deficiency is locked-in to
+//    *http.Server because of backward compatibility promises.
+//
 //  - If you use dlog, you don't have to manually configure the logging for the server to do the
 //    right thing.
 //
@@ -104,7 +133,7 @@ type testHookContextKey struct{}
 //    code's work.
 //
 // Arguably-breaking changes from http.Server that to ServerConfig that I'd say are bugfixes, but
-// could conceivably[1] make someone's old code incorrect:
+// could conceivably[2] make someone's old code incorrect:
 //
 //  - *http.Server.ServeTLS won't close the Listener if .ServeTLS returns early during setup due to
 //    having been passed invalid cert or key files; ServerConfig.ServeTLS will always close the
@@ -114,12 +143,11 @@ type testHookContextKey struct{}
 // functions that take an *http.Server as an argument) is that it became increasingly clear that the
 // lifecycle of a running server is tied to the lifecycle of the *http.Server object, while we've
 // grown a standard "Context" lifecycle system that wants to tie the lifecycle to the function call,
-// Rather than to the object.  This divorce between the running-server lifecycle and the object is
-// embodied in the name of the type; when the lifecycle of the object was the lifecycle of the
-// server, the object *was* the server, so it was named *http.Server; now the lifecycle is divorced
-// from that, and the object is just configuration for the server, so it is named *ServerConfig.
+// rather than to the object.  This divorce between lifecycles is embodied in the name of the type;
+// when the lifecycle was tied to the object the type was named *http.Server.  Now the lifecycle is
+// divorced from that, and the object is just configuration, so the type is named *ServerConfig.
 //
-// [1]: https://xkcd.com/1172/
+// [2]: https://xkcd.com/1172/
 type ServerConfig struct {
 	// These fields mimic exactly mimic http.Server; see the documentation there.
 	Handler           http.Handler
@@ -271,7 +299,7 @@ func (sc *ServerConfig) serve(ctx context.Context, serveFn func(*http.Server) er
 	}
 
 	// At this point, everything managed by the http.Server has finished, but hijacked
-	// connections may still be going.  We don't want to forcfully kill them if we haven't
+	// connections may still be going.  We don't want to forcefully kill them if we haven't
 	// actually had a hard shutdown triggered yet.  So wait for that to happen.
 	workersDoneCh := make(chan struct{})
 	go func() {
@@ -287,7 +315,8 @@ func (sc *ServerConfig) serve(ctx context.Context, serveFn func(*http.Server) er
 	}
 
 	// Trigger the hard shutdown.  This is probably not nescessary in the <-workersDoneCh case,
-	// but let's do it in both cases, just to be safe.
+	// but let's do it in both cases, just to be safe (the "close" calls should be safe no-ops
+	// in the <-workersDoneCh case).
 	//
 	// If the hardCtx becomes Done before server shuts down, then server.Shutdown() simply
 	// returns early, without doing any more-aggressive shutdown logic.  So we really do need to
@@ -307,22 +336,34 @@ func (sc *ServerConfig) serve(ctx context.Context, serveFn func(*http.Server) er
 	return err
 }
 
-// Serve accepts incoming connection on the Listener ln, creating a new service goroutine for each.
-// The service goroutines read requests and call sc.Handler to reply to them.
+// Serve accepts incoming connections on the Listener ln, creating a new worker goroutine for each.
+// The worker goroutines read requests and call sc.Handler to reply to them.
+//
+// When the Context ctx becomes Done, Serve starts shutting down and prepares to return; it closes
+// the listener, closes idle connections, and either (1) waits for any active connections to finish,
+// or (2) forcefully closes any active connections.  When using a vanilla Context, it forcefully
+// closes the connections; when using a dcontext "soft" Context, it will wait for the connections
+// when the Context becomes soft-Done, and will forcefully close the connections when the Context
+// becomes hard-Done.
+//
+// If the Context becomes Done and Serve is able to return without having to forcefully close any
+// active connections, then nil is returned.  If Server encounters an error and must stop serving,
+// or if Serve had to forcefully close any active connections during shutdown, then an error is
+// returned.
 //
 // Serve always closes the Listener before returning.
 func (sc *ServerConfig) Serve(ctx context.Context, ln net.Listener) error {
 	return sc.serve(ctx, func(srv *http.Server) error { return srv.Serve(ln) })
 }
 
-// Serve accepts incoming connection on the Listener ln, creating a new service goroutine for each.
-// The service goroutines perform TLS setup, and then read requests and call sc.Handler to reply to
-// them.
+// ServeTLS is like Serve, except that the worker goroutines perform TLS setup on the connection
+// before going in to their read-loop.
 //
-// Files containing a certificate and matching private key for the server must be provided if
-// neither the Server's TLSConfig.Certificates nor TLSConfig.GetCertificate are populated.  If the
-// certificate is signed by a certificate authority, the certFile should be the concatenation of the
-// server's certificate, any intermediates, and the CA's certificate.
+// Filenames containing a certificate and matching private key for the server must be provided if
+// neither the ServerConfig's TLSConfig.Certificates nor TLSConfig.GetCertificate are populated.  If
+// the certificate is signed by a certificate authority, the certFile should be the concatenation of
+// the server's certificate, any intermediates, and the CA's certificate.  If TLSConfig.Certificates
+// are TLSConfig.GetCertificate are populated, then you may pass empty strings as the filenames.
 //
 // ServeTLS always closes the Listener before returning (this is slightly different than
 // *http.Server.ServeTLS, which does not close the Listener if returning early during setup due to
--- dhttp/server_hijacked.go
+++ dhttp/server_hijacked.go
@@ -10,19 +10,40 @@ import (
 type connContextKey struct{}
 
 // configureHijackTracking configures (mutates) an *http.Server to provide slightly better tracking
-// of Hijack()ed connections.  It returns a 'close' function that closes all active hijacked
-// connections (you should call this when you call server.Close), and a 'wait' function that blocks
-// until all of the workers have quit (you should call this when you call server.Shutdown).
+// of Hijack()ed connections.
+//
+// Hijack()ed connections are connections for which the http.Server.Handler said to the *http.Server
+// "you know what, stop doing HTTP and let me use the raw TCP socket" (as when having negotiated an
+// upgrade from HTTP/1 to WebSockets or from HTTP/1 to HTTP/2).  It is a design deficiency in
+// *http.Server that the *http.Server totally just stops tracking connections when they get
+// Hijack()ed; failing to close them when you call *http.Server.Close() and failing to wait for them
+// when you call *http.Server.Shutdown(); and unfortunately that design deficiency is locked-in to
+// *http.Server because of backward compatibility promises.
+//
+// So configureHijackTracking addresses that deficiency, and adds hooks in to the *http.Server in
+// order to do its own tracking of the Hijack()ed connections, and returns two functions that will
+// perform those two deficient operations for Hijack()ed connections.  It returns a 'close' function
+// that closes all active Hijack()ed connections (you should call this when you call server.Close),
+// and a 'wait' function that blocks until all of the workers have quit (you should call this
+// immediately after you call server.Shutdown).
 //
 // This wraps the server.Handler, so it should be called *after* setting up any Handler that might
-// hijack connections.
+// Hijack() connections.
 func configureHijackTracking(server *http.Server) (close func(), wait func()) {
 	var wg sync.WaitGroup
 
 	var mu sync.Mutex                            // protects 'hijackedConns'
 	hijackedConns := make(map[net.Conn]struct{}) // protected by 'mu'
+	closeHijacked := func() {
+		mu.Lock()
+		defer mu.Unlock()
+		for conn := range hijackedConns {
+			conn.Close()
+			delete(hijackedConns, conn)
+		}
+	}
 
-	// Make a note of it whenever a connection gets hijacked.
+	// Hook in to .ConnState in order to make a note of it whenever a connection gets hijacked.
 	origConnState := server.ConnState
 	server.ConnState = func(conn net.Conn, state http.ConnState) {
 		if origConnState != nil {
@@ -35,7 +56,8 @@ func configureHijackTracking(server *http.Server) (close func(), wait func()) {
 		}
 	}
 
-	// Pack the net.Conn in to the context, so that we can access it below.
+	// Hook in to .ConnContext in order to pack the net.Conn in to the Context, so that we can
+	// access it below.
 	server.ConnContext = concatConnContext(
 		func(ctx context.Context, c net.Conn) context.Context {
 			return context.WithValue(ctx, connContextKey{}, c)
@@ -43,9 +65,9 @@ func configureHijackTracking(server *http.Server) (close func(), wait func()) {
 		server.ConnContext,
 	)
 
-	// (1) Make a note of it whenever a hijacked connection's worker returns, so that we don't
-	// need to keep track of that connection forever. (2) Keep track of whether there are still
-	// outstanding workers.
+	// Hook in to .Handler in order to (1) make a note of it whenever a hijacked connection's
+	// worker returns, so that we don't need to keep track of that connection forever; and (2)
+	// keep track of whether there are still outstanding workers.
 	origHandler := server.Handler
 	if origHandler == nil {
 		origHandler = http.DefaultServeMux
@@ -62,14 +84,6 @@ func configureHijackTracking(server *http.Server) (close func(), wait func()) {
 		origHandler.ServeHTTP(w, r)
 	})
 
-	closeHijacked := func() {
-		mu.Lock()
-		defer mu.Unlock()
-		for conn := range hijackedConns {
-			conn.Close()
-			delete(hijackedConns, conn)
-		}
-	}
-
+	// Return.
 	return closeHijacked, wg.Wait
 }
