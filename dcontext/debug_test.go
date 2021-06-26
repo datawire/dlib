1263db8c3201553acafc2af225bbcaaeaf7dcd49
Luke Shumaker <lukeshu@datawire.io>
2021-06-26 16:10:20 -0600

    dcontext: Add a WithoutCancel function for non-timed-out cleanup
    The idea+implementation is borrowed from Telepresence.  The tests are new.
    
    A long time ago I had a conversation with Rafael Schloming about "what
    about non-timed-out cleanup?" in the context of dgroup.
    
    Well, much later, it turned out that Telepresence's answer to that was
    withoutCancel{}.  So let's put that answer into dlib.

--- dcontext/debug_test.go
+++ dcontext/debug_test.go
@@ -12,17 +12,39 @@ import (
 )
 
 func TestDebug(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	baseStr := fmt.Sprint(ctx)
-	ctx = dcontext.WithSoftness(ctx)
-
-	softStr := fmt.Sprint(ctx)
-	hardStr := fmt.Sprint(dcontext.HardContext(ctx))
-
-	assert.Truef(t, strings.HasPrefix(softStr, baseStr+"."),
-		"Soft Context string does not start with %q: %q", baseStr+".", softStr)
-	assert.Equal(t,
-		softStr+".HardContext",
-		hardStr)
+	ctx := context.Background()
+	var cancel context.CancelFunc
+	testcases := []struct {
+		Name   string
+		SetCtx func()
+		Suffix string
+	}{
+		{
+			Name: "WithCancel",
+			SetCtx: func() {
+				ctx, cancel = context.WithCancel(ctx)
+				t.Cleanup(cancel)
+			},
+			Suffix: "WithCancel",
+		},
+		{"WithSoftness", func() { ctx = dcontext.WithSoftness(ctx) }, ""},
+		{"HardContext", func() { ctx = dcontext.HardContext(ctx) }, "HardContext"},
+		{"WithoutCancel", func() { ctx = dcontext.WithoutCancel(ctx) }, "WithoutCancel"},
+	}
+	t.Log(fmt.Sprint(ctx))
+	for _, tc := range testcases {
+		tc := tc
+		t.Run(tc.Name, func(t *testing.T) {
+			before := fmt.Sprint(ctx)
+			tc.SetCtx()
+			after := fmt.Sprint(ctx)
+			t.Log(after)
+			if tc.Suffix == "" {
+				assert.Truef(t, strings.HasPrefix(after, before+"."),
+					"%s string does not start with %q: %q", tc.Name, before+".", after)
+			} else {
+				assert.Equal(t, before+"."+tc.Suffix, after)
+			}
+		})
+	}
 }
