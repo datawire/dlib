fa298ef6347855ff7ab597d389298f8b065522c1
Luke Shumaker <lukeshu@datawire.io>
2021-08-06 20:36:13 -0600

    dcontext: Fix a bug where HardContext(WithoutCancel(ctx)) gets canceled

--- dcontext/without_cancel.go
+++ dcontext/without_cancel.go
@@ -13,6 +13,12 @@ func (withoutCancel) Deadline() (deadline time.Time, ok bool) { return }
 func (withoutCancel) Done() <-chan struct{}                   { return nil }
 func (withoutCancel) Err() error                              { return nil }
 func (c withoutCancel) String() string                        { return contextName(c.Context) + ".WithoutCancel" }
+func (c withoutCancel) Value(key interface{}) interface{} {
+	if key == (parentHardContextKey{}) {
+		return nil
+	}
+	return c.Context.Value(key)
+}
 
 // WithoutCancel returns a copy of parent that inherits only values and not
 // deadlines/cancellation/errors.  This is useful for implementing non-timed-out
