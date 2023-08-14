8616ff6f3364000704fcf4d5e22887aa84b7b3d1
Thomas Hallgren <thomas@datawire.io>
2023-08-14 11:09:04 +0200

    Turn example into proper example test.

--- dcontext/hardsoft_example_test.go
+++ dcontext/hardsoft_example_test.go
@@ -2,6 +2,7 @@ package dcontext_test
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/datawire/dlib/dcontext"
@@ -10,9 +11,10 @@ import (
 
 // This should be a very simple example of a parent caller function, showing how
 // to manage a hard/soft Context and how to call code that is dcontext-aware.
-func Example_caller() error {
-	ctx := context.Background()                       // Context is hard by default
-	ctx, timeToDie := context.WithCancel(ctx)         // hard Context => hard cancel
+func ExampleWithSoftness() {
+	ctx := context.Background()               // Context is hard by default
+	ctx, timeToDie := context.WithCancel(ctx) // hard Context => hard cancel
+	defer timeToDie()
 	ctx = dcontext.WithSoftness(ctx)                  // make it soft
 	ctx, startShuttingDown := context.WithCancel(ctx) // soft Context => soft cancel
 
@@ -21,11 +23,11 @@ func Example_caller() error {
 		sc := &dhttp.ServerConfig{
 			// ...
 		}
-		retCh <- sc.ListenAndServe(ctx, ":http")
+		retCh <- sc.ListenAndServe(ctx, ":0")
 	}()
 
 	// Run for a while.
-	time.Sleep(10 * time.Second)
+	time.Sleep(3 * time.Second)
 
 	// Shut down.
 	startShuttingDown() // Soft shutdown; start draining connections.
@@ -34,13 +36,21 @@ func Example_caller() error {
 		// It shut down fine with just the soft shutdown; everything was
 		// well-behaved.  It isn't necessary to cut shutdown short by
 		// triggering a hard shutdown with timeToDie() in this case.
-		return err
-	case <-time.After(5 * time.Second): // shutdown grace period
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			fmt.Println("soft shutdown")
+		}
+	case <-time.After(2 * time.Second): // shutdown grace period
 		// It's taking too long to shut down--it seems that some clients
 		// are refusing to hang up.  So now we trigger a hard shutdown
 		// and forcefully close the connections.  This will cause errors
 		// for those clients.
 		timeToDie() // Hard shutdown; cause errors for clients
-		return <-retCh
+		if err := <-retCh; err != nil {
+			fmt.Println(err.Error())
+		}
 	}
+	// Output:
+	// soft shutdown
 }
